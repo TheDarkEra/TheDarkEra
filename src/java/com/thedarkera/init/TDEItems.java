9652b49a3ebdde3cd953d70e7eadbf624be0ce6c
Emmanuel Maximus Yohanes <geofanny.emmanuel@yahoo.co.id>
2015-04-15 16:23:47 +0700

    Added textures for soul gems

--- src/java/com/thedarkera/init/TDEItems.java
+++ src/java/com/thedarkera/init/TDEItems.java
@@ -8,11 +8,14 @@ import net.minecraft.item.Item;
 public class TDEItems
 {
 	public static Item common_soul_gem;
+	public static Item lesser_soul_gem;
 	
 	public static void init()
 	{
 		common_soul_gem = new Item().setUnlocalizedName("common_soul_gem").setTextureName(TheDarkEra.MODID + ":common_soul_gem").setCreativeTab(TheDarkEra.tabTDE);
+		lesser_soul_gem = new Item().setUnlocalizedName("lesser_soul_gem").setTextureName(TheDarkEra.MODID + ":lesser_soul_gem").setCreativeTab(TheDarkEra.tabTDE);
 		
 		GameRegistry.registerItem(common_soul_gem, common_soul_gem.getUnlocalizedName());
+		GameRegistry.registerItem(lesser_soul_gem, lesser_soul_gem.getUnlocalizedName());
 	}
 }
