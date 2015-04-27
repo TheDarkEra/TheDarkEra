095c1f38036f3492ee4512befc6188fc57fae6f4
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-04-27 20:05:02 +0200

    Changed the helm postion

--- src/java/com/thedarkera/init/TDERecipes.java
+++ src/java/com/thedarkera/init/TDERecipes.java
@@ -1,9 +1,18 @@
 package com.thedarkera.init;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+
 public class TDERecipes 
 {
 	public static void init() 
 	{
-		
+		GameRegistry.addRecipe(new ItemStack(TDEItems.leather_strip),
+			   	"XXX",
+			    "XXX",
+			    "XXX",
+			    'X', Blocks.leaves
+			);
 	}
 }
