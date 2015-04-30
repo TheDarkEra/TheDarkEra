d3702cdff3858dd2533ecec917aa29e44c6401f2
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-04-30 22:20:41 +0200

    GuiHandler

---
+++ src/java/com/thedarkera/handler/GuiHandler.java
@@ -0,0 +1,22 @@
+package com.thedarkera.handler;
+
+import com.thedarkera.TheDarkEra.GUIs;
+import com.thedarkera.gui.GuiWorkBench;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class GuiHandler implements IGuiHandler {
+
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        if(ID == GUIs.WORKBENCH.ordinal()) return new GuiWorkBench(player, world, x, y, z);
+        else return null;
+    }
+}
