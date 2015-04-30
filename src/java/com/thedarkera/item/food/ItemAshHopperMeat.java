9c64aff105e36d6a86c1534c8c1c0bf413508d89
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-04-30 23:21:47 +0200

    Named some blocks on the lang

--- src/java/com/thedarkera/item/food/ItemAshHopperMeat.java
+++ src/java/com/thedarkera/item/food/ItemAshHopperMeat.java
@@ -1,17 +1,10 @@
 package com.thedarkera.item.food;
 
+import net.minecraft.item.ItemFood;
+
 import com.thedarkera.TheDarkEra;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.world.World;
-
 public class ItemAshHopperMeat extends ItemFood{
-
 
 	public ItemAshHopperMeat(int food, int saturation, boolean wolfFood, String name) {
 		super(food, saturation, wolfFood);
