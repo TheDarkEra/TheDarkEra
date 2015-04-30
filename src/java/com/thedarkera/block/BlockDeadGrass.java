231bdf6586217e86e38a06e81f53b75966aa13b0
iMrBlurr <imrblurr@gmail.com>
2015-04-30 19:46:22 +0100

    Made Dead Grass multi textured

--- src/java/com/thedarkera/block/BlockDeadGrass.java
+++ src/java/com/thedarkera/block/BlockDeadGrass.java
@@ -1,12 +1,16 @@
 package com.thedarkera.block;
 
-import com.thedarkera.TheDarkEra;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+
+import com.thedarkera.TheDarkEra;
 
 public class BlockDeadGrass extends Block{
 
+	private IIcon[] icons = new IIcon[6];
+	
 	String name = "dead_grass";
 	
 	public BlockDeadGrass(){
@@ -19,6 +23,32 @@ public class BlockDeadGrass extends Block{
 		setHarvestLevel("shovel", 0);
 	}
 	
+	@Override
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		for (int i = 0; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon("dirt");
+	    }
+		for (int i = 1; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon(TheDarkEra.MODID + ":dead_grass");
+	    }
+		for (int i = 2; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon("dirt");
+	    }
+		for (int i = 3; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon("dirt");
+	    }
+		for (int i = 4; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon("dirt");
+	    }
+		for (int i = 5; i < icons.length; i++) {
+	        icons[i] = iconRegister.registerIcon("dirt");
+	    }
+	}
+	
+	@Override
+	public IIcon getIcon(int side, int meta) {
+	    return icons[side];
+	}
 	/**
 	 * Future:
 	 * Needs to have dirt texture on sides, bottom and dead_grass on top.
