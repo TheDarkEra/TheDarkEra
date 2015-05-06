b1213b969bf0b87ad5e38217aa8c4c98b9628098
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-05-06 19:14:18 +0200

    Steel Ore Texture

--- src/java/com/thedarkera/world/biome/BiomeDarkForrest.java
+++ src/java/com/thedarkera/world/biome/BiomeDarkForrest.java
@@ -2,7 +2,6 @@ package com.thedarkera.world.biome;
 
 import java.util.Random;
 
-import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.gen.feature.WorldGenDesertWells;
@@ -10,13 +9,11 @@ import com.thedarkera.init.TDEBlocks;
 
 public class BiomeDarkForrest extends BiomeGenBase{
 
-	Block stoneBlock = TDEBlocks.dark_stone;
 	
 	public BiomeDarkForrest(int biomeID) {
 		super(biomeID);
 		topBlock = TDEBlocks.dark_grass;
 		fillerBlock = TDEBlocks.dark_dirt;
-		waterColorMultiplier = 0xE42D17;
 		spawnableCreatureList.clear();
 		spawnableMonsterList.clear();
 		spawnableWaterCreatureList.clear();
@@ -42,7 +39,6 @@ public class BiomeDarkForrest extends BiomeGenBase{
 	/*ToDo: 
 	 *Need to add trees
 	 *Bushes
-	 *Dark Stone replace stone
 	 *and more decorative blocks. 
 	 *
 	 *NEED MORE BIOME BLOCKS
