00d4b1895d61294f1d06bd90102d2579403fd4bd
iMrBlurr <imrblurr@gmail.com>
2015-05-06 11:57:53 +0100

    BlockRotatedPillar but needs fixing
    needs fixing but doesn't crash

--- src/java/com/thedarkera/block/BlockRottenWoodLog.java
+++ src/java/com/thedarkera/block/BlockRottenWoodLog.java
@@ -1,20 +1,21 @@
 package com.thedarkera.block;
 
-import net.minecraft.block.BlockLog;
+import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.util.IIcon;
 
 import com.thedarkera.TheDarkEra;
 
-public class BlockRottenWoodLog extends BlockLog
+public class BlockRottenWoodLog extends BlockRotatedPillar
 {
 	private IIcon[] icons = new IIcon[6];
 	String name = "rotten_wood_log";
+	private String texture = "rotten_wood_log";
 
 	public BlockRottenWoodLog(Material m)
 	{
-		super();
+		super(m);
 		setCreativeTab(TheDarkEra.tabTDE);
 		setBlockName(name);
 		setBlockTextureName(name);
@@ -28,13 +29,21 @@ public class BlockRottenWoodLog extends BlockLog
 	@Override
 	public void registerBlockIcons(IIconRegister iconRegister)
 	{
-		for (int i = 0; i < icons.length; i++) {
-			icons[i] = iconRegister.registerIcon("log_oak");
-		}
-		icons[2] = iconRegister.registerIcon("log_oak_top");
-		icons[3] = iconRegister.registerIcon("log_oak_top");
+		/*icons[0] = iconRegister.registerIcon(TheDarkEra.MODID + ":" + texture + "_top");
+		icons[1] = iconRegister.registerIcon(TheDarkEra.MODID + ":" + texture + "_top");
+		icons[2] = iconRegister.registerIcon(TheDarkEra.MODID + ":" + texture);*/
+		
+		
+		icons[0] = iconRegister.registerIcon("log_oak" + "_top");
+		icons[1] = iconRegister.registerIcon("log_oak" + "_top");
+		icons[2] = iconRegister.registerIcon("log_oak");
 	}
 
+	@Override
+	protected IIcon getSideIcon(int p_150163_1_) {
+		return icons[2];
+	}
+	
 	@Override
 	public IIcon getIcon(int side, int meta) {
 		return icons[side];
