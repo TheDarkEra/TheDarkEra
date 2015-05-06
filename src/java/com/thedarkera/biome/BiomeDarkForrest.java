7ba5835b30933e8623334a4ae36021fa202d177f
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-05-06 16:38:15 +0200

    Ancient Ore Texture

--- src/java/com/thedarkera/biome/BiomeDarkForrest.java
+++ src/java/com/thedarkera/biome/BiomeDarkForrest.java
@@ -5,7 +5,6 @@ import java.util.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.gen.feature.WorldGenDesertWells;
-
 import com.thedarkera.init.TDEBlocks;
 
 public class BiomeDarkForrest extends BiomeGenBase{
