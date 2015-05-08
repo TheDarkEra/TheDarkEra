795a5b383d29e76b19dc665a78b0cb931483d1a6
iMrBlurr <imrblurr@gmail.com>
2015-05-08 16:15:15 +0100

    Format Recipe

--- src/java/com/thedarkera/init/TDERecipes.java
+++ src/java/com/thedarkera/init/TDERecipes.java
@@ -6,24 +6,24 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class TDERecipes {
 	public static void init() {
-		// Blocks//		
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.smeltery), "xxx", "x x", "xxx", 'x', TDEBlocks.dark_cobblestone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_bricks, 4),"xx", "xx", 'x', TDEBlocks.dark_stone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_stairs, 6),"x  ", "xx ", "xxx", 'x',TDEBlocks.dark_stone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_stairs, 6), "  x", " xx", "xxx", 'x',TDEBlocks.dark_stone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_stairs, 6),"  x", " xx", "xxx", 'x',TDEBlocks.rotten_wood );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_stairs, 6), "x  ", "xx ", "xxx", 'x',TDEBlocks.rotten_wood );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_slab, 3), "xxx", 'x', TDEBlocks.dark_stone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_slab, 3), "xxx", 'x', TDEBlocks.rotten_wood );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_fence, 3), "xxx", "xxx", 'x', TDEItems.dark_stone_rod );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_fence, 3), "xxx", "xxx", 'x', TDEItems.rotten_wood_stick );
+		// Blocks//
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.smeltery), "xxx", "x x", "xxx", 'x', TDEBlocks.dark_cobblestone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_bricks, 4), "xx", "xx", 'x', TDEBlocks.dark_stone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_stairs, 6), "x  ", "xx ", "xxx", 'x', TDEBlocks.dark_stone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_stairs, 6), "  x", " xx", "xxx", 'x', TDEBlocks.dark_stone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_stairs, 6), "  x", " xx", "xxx", 'x', TDEBlocks.rotten_wood);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_stairs, 6), "x  ", "xx ", "xxx", 'x', TDEBlocks.rotten_wood);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_slab, 3), "xxx", 'x', TDEBlocks.dark_stone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_slab, 3), "xxx", 'x', TDEBlocks.rotten_wood);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.dark_stone_fence, 3), "xxx", "xxx", 'x', TDEItems.dark_stone_rod);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEBlocks.rotten_wood_fence, 3), "xxx", "xxx", 'x', TDEItems.rotten_wood_stick);
 
 		GameRegistry.addSmelting(TDEBlocks.dark_cobblestone, new ItemStack(TDEBlocks.dark_stone), 0.2F);
 
 		// Misc items//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.leather_strip, 2), "  ", " x ", "  ", 'x', Items.leather );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.dark_stone_rod, 4), "x", "x", 'x', TDEBlocks.dark_cobblestone );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.rotten_wood_stick, 4), "x", "x", 'x', TDEBlocks.rotten_wood );
+		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.leather_strip, 2), "  ", " x ", "  ", 'x', Items.leather);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.dark_stone_rod, 4), "x", "x", 'x', TDEBlocks.dark_cobblestone);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEItems.rotten_wood_stick, 4), "x", "x", 'x', TDEBlocks.rotten_wood);
 
 		// Ingots//
 		GameRegistry.addSmelting(Items.chainmail_boots, new ItemStack(TDEItems.steel_ingot, 2), 0.8F);
@@ -37,49 +37,49 @@ public class TDERecipes {
 
 		// Armors//
 		// Daedric armor//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_helmet), "xxx", "y y", 'x', TDEItems.ebony_ingot, 'y',	TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.ebony_ingot,'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_leggings), "xxx", "x x", "y y", 'x', TDEItems.ebony_ingot, 'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_boots),"x x", "y y", 'x', TDEItems.ebony_ingot, 'y',TDEItems.leather_strip );
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_helmet), "xxx", "y y", 'x', TDEItems.ebony_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.ebony_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_leggings), "xxx", "x x", "y y", 'x', TDEItems.ebony_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.daedric_boots), "x x", "y y", 'x', TDEItems.ebony_ingot, 'y', TDEItems.leather_strip);
+
 		// Steel armor//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_helmet), "xxx", "y y", 'x', TDEItems.steel_ingot, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.steel_ingot,	'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_leggings), "xxx", "x x", "y y", 'x', TDEItems.steel_ingot,'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_boots), "x x", "y y", 'x', TDEItems.steel_ingot, 'y',TDEItems.leather_strip );
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_helmet), "xxx", "y y", 'x', TDEItems.steel_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.steel_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_leggings), "xxx", "x x", "y y", 'x', TDEItems.steel_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.steel_boots), "x x", "y y", 'x', TDEItems.steel_ingot, 'y', TDEItems.leather_strip);
+
 		// Ancient armor//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_helmet), "xxx", "y y", 'x', TDEItems.ancient_ingot, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.ancient_ingot, 'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_leggings), "xxx", "x x", "y y", 'x',TDEItems.ancient_ingot, 'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_boots), "x x", "y y", 'x', TDEItems.ancient_ingot, 'y',TDEItems.leather_strip );
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_helmet), "xxx", "y y", 'x', TDEItems.ancient_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.ancient_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_leggings), "xxx", "x x", "y y", 'x', TDEItems.ancient_ingot, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.ancient_boots), "x x", "y y", 'x', TDEItems.ancient_ingot, 'y', TDEItems.leather_strip);
+
 		// Dragon heavy armor//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_helmet), "xxx", "y y", 'x', TDEItems.dragon_bone, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.dragon_bone, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_leggings),"xxx", "x x", "y y", 'x', TDEItems.dragon_bone,	'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_boots), "x x", "y y", 'x', TDEItems.dragon_bone, 'y',TDEItems.leather_strip );
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_helmet), "xxx", "y y", 'x', TDEItems.dragon_bone, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.dragon_bone, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_leggings), "xxx", "x x", "y y", 'x', TDEItems.dragon_bone, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_heavy_boots), "x x", "y y", 'x', TDEItems.dragon_bone, 'y', TDEItems.leather_strip);
+
 		// Dragon light armor//
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_helmet), "xxx", "y y", 'x', TDEItems.dragon_scale, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_chestplate),"y y", "xxx", "xxx", 'x', TDEItems.dragon_scale, 'y',TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_leggings),"xxx", "x x", "y y", 'x', TDEItems.dragon_scale,'y', TDEItems.leather_strip );
-		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_boots), "x x", "y y", 'x', TDEItems.dragon_scale, 'y',TDEItems.leather_strip );
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_helmet), "xxx", "y y", 'x', TDEItems.dragon_scale, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_chestplate), "y y", "xxx", "xxx", 'x', TDEItems.dragon_scale, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_leggings), "xxx", "x x", "y y", 'x', TDEItems.dragon_scale, 'y', TDEItems.leather_strip);
+		GameRegistry.addShapedRecipe(new ItemStack(TDEArmors.dragon_light_boots), "x x", "y y", 'x', TDEItems.dragon_scale, 'y', TDEItems.leather_strip);
 
 		// Tools//
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_pickaxe), "xxx", " y ", " y ", 'x', TDEItems.steel_ingot,	'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_pickaxe),  "xxx", " y ", " y ", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_axe),  "xx", "xy", " y", 'x', TDEItems.steel_ingot, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_axe), "xx", "yx", "y ", 'x', TDEItems.steel_ingot, 'y',Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_axe), "xx ", "xy ", " y ", 'x',TDEBlocks.dark_cobblestone, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_axe),"xx", "yx", "y ", 'x',TDEBlocks.dark_cobblestone, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_shovel), "x", "y", "y", 'x', TDEBlocks.dark_cobblestone,'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_shovel), "x", "y", "y", 'x', TDEItems.steel_ingot, 'y',Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_hoe), "xx", "y ", "y ", 'x',	TDEBlocks.dark_cobblestone, 'y', Items.stick );
-    	GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_hoe),"xx", " y", " y", 'x',TDEBlocks.dark_cobblestone, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_hoe), "xx", " y", " y", 'x', TDEItems.steel_ingot, 'y', Items.stick );
-		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_hoe), "xx", "y ", "y ", 'x', TDEItems.steel_ingot, 'y', Items.stick );
-		
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_pickaxe), "xxx", " y ", " y ", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_pickaxe), "xxx", " y ", " y ", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_axe), "xx", "xy", " y", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_axe), "xx", "yx", "y ", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_axe), "xx ", "xy ", " y ", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_axe), "xx", "yx", "y ", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_shovel), "x", "y", "y", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_shovel), "x", "y", "y", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_hoe), "xx", "y ", "y ", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.dark_stone_hoe), "xx", " y", " y", 'x', TDEBlocks.dark_cobblestone, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_hoe), "xx", " y", " y", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+		GameRegistry.addShapedRecipe(new ItemStack(TDETools.steel_hoe), "xx", "y ", "y ", 'x', TDEItems.steel_ingot, 'y', Items.stick);
+
 		GameRegistry.addSmelting(TDEBlocks.dark_iron_ore, new ItemStack(Items.iron_ingot), 1F);
 		GameRegistry.addSmelting(TDEBlocks.dark_gold_ore, new ItemStack(Items.gold_ingot), 1F);
 	}
