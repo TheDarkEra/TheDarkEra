e02f1ff92864bf30b375a1821904394dc9a828f7
Matthijs <matthijs.booman@hotmail.com>
2015-05-12 23:12:36 +0200

    added dark_fire texture
    will fix the grey parts tomorrow

--- src/java/com/thedarkera/block/BlockDarkFire.java
+++ src/java/com/thedarkera/block/BlockDarkFire.java
@@ -13,7 +13,8 @@ public class BlockDarkFire extends BlockFire {
 
     public BlockDarkFire() {
         super();
-        this.setBlockName("darkfire");
+        this.setBlockName("dark_fire");
+        this.setBlockTextureName(TheDarkEra.MODID + ":" + "dark_fire");
     }
 
     public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
