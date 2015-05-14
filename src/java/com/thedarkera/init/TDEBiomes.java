f33e5787f2fa999352e92249ca0b5a72ac00499c
Matthijs <matthijs.booman@hotmail.com>
2015-05-14 18:17:34 +0200

    more biome related stuff

--- src/java/com/thedarkera/init/TDEBiomes.java
+++ src/java/com/thedarkera/init/TDEBiomes.java
@@ -1,5 +1,6 @@
 package com.thedarkera.init;
 
+import com.thedarkera.world.biome.BiomeDarkHills;
 import com.thedarkera.world.biome.BiomeDarkOcean;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.BiomeManager;
@@ -12,20 +13,20 @@ public class TDEBiomes {
     public static BiomeGenBase dark_ocean;
 	public static BiomeGenBase dark_land;
 	public static BiomeGenBase dark_forrest;
-	public static BiomeGenBase dark_village;
+	public static BiomeGenBase dark_village; //dark village biome???
 	public static BiomeGenBase dark_beach;
 	public static BiomeGenBase dark_hills;
-	public static BiomeGenBase test_biome;
+	//public static BiomeGenBase test_biome;
 
 	public static void init() {
 
-        dark_ocean = (new BiomeDarkOcean(51)).setBiomeName("Ocean");// we don't want this to be a spawn biome, do we?
-		dark_land = (new BiomeDarkLand(52)).setColor(9286496).setBiomeName("dark_land");
+        dark_ocean = (new BiomeDarkOcean(51)).setBiomeName("Dark Ocean");
+		dark_land = (new BiomeDarkLand(52)).setColor(9286496).setBiomeName("Dark Land");
 		BiomeManager.addSpawnBiome(dark_land);
-		dark_forrest = (new BiomeDarkForrest(53)).setColor(9286496).setBiomeName("dark_forrest");
+		dark_forrest = (new BiomeDarkForrest(53)).setColor(9286496).setBiomeName("Dark Forrest");
 		BiomeManager.addSpawnBiome(dark_forrest);
-
-
+        dark_hills = (new BiomeDarkHills(54)).setColor(6316128).setBiomeName("Dark Extreme Hills");
+        BiomeManager.addSpawnBiome(dark_hills);
 	}
 
 }
---
+++ src/java/com/thedarkera/world/biome/BiomeDarkHills.java
@@ -0,0 +1,122 @@
+package com.thedarkera.world.biome;
+
+import com.thedarkera.init.TDEBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+
+import java.util.Random;
+
+public class BiomeDarkHills extends BiomeGenBase {
+
+    public BiomeDarkHills(int id){
+        super(id);
+        this.topBlock = TDEBlocks.dark_grass;
+        this.fillerBlock = TDEBlocks.dark_dirt;
+        spawnableCreatureList.clear();
+        spawnableMonsterList.clear();
+        spawnableWaterCreatureList.clear();
+        waterColorMultiplier = 0x000014;
+        setHeight(height_MidHills);
+        setTemperatureRainfall(0.2F, 0.3F);
+    }
+
+    public void genTerrainBlocks(World world, Random rand, Block[] p_150560_3_, byte[] p_150560_4_, int p_150560_5_, int p_150560_6_, double p_150560_7_)
+    {
+        boolean flag = true;
+        Block block = this.topBlock;
+        byte b0 = (byte)(this.field_150604_aj & 255);
+        Block block1 = this.fillerBlock;
+        int k = -1;
+        int l = (int)(p_150560_7_ / 3.0D + 3.0D + rand.nextDouble() * 0.25D);
+        int i1 = p_150560_5_ & 15;
+        int j1 = p_150560_6_ & 15;
+        int k1 = p_150560_3_.length / 256;
+
+        for (int l1 = 255; l1 >= 0; --l1)
+        {
+            int i2 = (j1 * 16 + i1) * k1 + l1;
+
+            if (l1 <= 0 + rand.nextInt(5))
+            {
+                p_150560_3_[i2] = Blocks.bedrock;
+            }
+            else
+            {
+                Block block2 = p_150560_3_[i2];
+
+                if (block2 != null && block2.getMaterial() != Material.air)
+                {
+                    if (block2 == TDEBlocks.dark_stone)
+                    {
+                        if (k == -1)
+                        {
+                            if (l <= 0)
+                            {
+                                block = null;
+                                b0 = 0;
+                                block1 = TDEBlocks.dark_stone;
+                            }
+                            else if (l1 >= 59 && l1 <= 64)
+                            {
+                                block = this.topBlock;
+                                b0 = (byte)(this.field_150604_aj & 255);
+                                block1 = this.fillerBlock;
+                            }
+
+                            if (l1 < 63 && (block == null || block.getMaterial() == Material.air))
+                            {
+                                if (this.getFloatTemperature(p_150560_5_, l1, p_150560_6_) < 0.15F)
+                                {
+                                    block = Blocks.ice;
+                                    b0 = 0;
+                                }
+                                else
+                                {
+                                    block = Blocks.water;
+                                    b0 = 0;
+                                }
+                            }
+
+                            k = l;
+
+                            if (l1 >= 62)
+                            {
+                                p_150560_3_[i2] = block;
+                                p_150560_4_[i2] = b0;
+                            }
+                            else if (l1 < 56 - l)
+                            {
+                                block = null;
+                                block1 = TDEBlocks.dark_stone;
+                                p_150560_3_[i2] = Blocks.gravel;
+                            }
+                            else
+                            {
+                                p_150560_3_[i2] = block1;
+                            }
+                        }
+                        else if (k > 0)
+                        {
+                            --k;
+                            p_150560_3_[i2] = block1;
+
+                            if (k == 0 && block1 == Blocks.sand)
+                            {
+                                k = rand.nextInt(4) + Math.max(0, l1 - 63);
+                                block1 = Blocks.sandstone;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    k = -1;
+                }
+            }
+        }
+    }
+
+}
