5737c0365b297d5de8e265987ec0b9f1b94cd85e
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-05-15 13:09:30 +0200

    Dimesion darker

--- src/java/com/thedarkera/world/TDEWorldProvider.java
+++ src/java/com/thedarkera/world/TDEWorldProvider.java
@@ -1,7 +1,9 @@
 package com.thedarkera.world;
 
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.WorldChunkManagerHell;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -11,13 +13,18 @@ import com.thedarkera.TheDarkEra;
 import com.thedarkera.init.TDEBiomes;
 import com.thedarkera.world.renderers.SkyRenderer;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class TDEWorldProvider extends WorldProvider {
 
 	@Override
 	public void registerWorldChunkManager() {
 	
 		this.worldChunkMgr = new WorldChunkManagerTDE(worldObj.getSeed(), terrainType);
-		this.dimensionId = TheDarkEra.dimension;
+		this.dimensionId = TheDarkEra.dimension;	
+	//	this.isHellWorld = true;
+	    this.hasNoSky = true;
 	}
 
 	public boolean isSurfaceWorld() {
@@ -43,7 +50,6 @@ public class TDEWorldProvider extends WorldProvider {
 	public boolean canRespawnHere() {
 		return true;
 	}
-
 	@Override
 	public IChunkProvider createChunkGenerator() {
 		return new TDEChunkProvider(worldObj, worldObj.getSeed(), true);
@@ -53,6 +59,13 @@ public class TDEWorldProvider extends WorldProvider {
 	public String getDimensionName() {
 		return "The Dark Dimension";
 	}
+	@Override
+	@SideOnly(Side.CLIENT)
+	    public float[] calcSunriseSunsetColors(float p_76560_1_, float p_76560_2_)
+	    {
+	        return null;
+	    }
+
 
 	@Override
 	public ChunkCoordinates getEntrancePortalLocation() {
@@ -63,15 +76,32 @@ public class TDEWorldProvider extends WorldProvider {
 	public boolean canDoLightning(Chunk chunk) {
 		return true;
 	}
-	
 	@Override
 	public boolean canDoRainSnowIce(Chunk chunk) {
-		return false;
+		return true;
 
 	}
-
 	@Override
-	public IRenderHandler getSkyRenderer() {
-		return new SkyRenderer();
-	}
+	@SideOnly(Side.CLIENT)
+	    public boolean isSkyColored()
+	    {
+	        return false;
+	    }
+	@Override
+	  protected void generateLightBrightnessTable()
+	    {
+	        float f = 0.1F;
+
+	        for (int i = 0; i <= 15; ++i)
+	        {
+	            float f1 = 1.0F - (float)i / 15.0F;
+	            this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * (1.0F - f) + f;
+	        }
+	    }
+
+	//@Override
+//	public IRenderHandler getSkyRenderer() {
+	//	return new SkyRenderer();
+	//}
+
 }
