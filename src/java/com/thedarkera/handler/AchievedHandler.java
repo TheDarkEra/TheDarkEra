a4c2a5c926cd274ec71d07acc9dea6af69b45932
iMrBlurr <imrblurr@gmail.com>
2015-05-27 21:16:08 +0100

    updated stupid mistake

--- src/java/com/thedarkera/handler/AchievedHandler.java
+++ src/java/com/thedarkera/handler/AchievedHandler.java
@@ -4,17 +4,16 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+
 import com.thedarkera.init.TDEAchievements;
 import com.thedarkera.init.TDETools;
 import com.thedarkera.init.TDEWeapons;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
-
+import cpw.mods.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
 
 public class AchievedHandler {
-
-	// player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "achievement"));
-	// AchievementHandler.setAchieved(TDEAchievements.biome);
 
 	@SubscribeEvent
 	public void onCrafted(ItemCraftedEvent event) {
@@ -25,22 +24,24 @@ public class AchievedHandler {
 			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Stone Upgrade!"));
 			AchievementHandler.setAchieved(TDEAchievements.stone_upgrade);
 		}
-		
+
 		if (item.equals(TDETools.steel_pickaxe) || item.equals(TDETools.steel_axe) || item.equals(TDETools.steel_shovel) || item.equals(TDETools.steel_hoe) || item.equals(TDEWeapons.steel_sword)) {
 			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Steel Upgrade!"));
 			AchievementHandler.setAchieved(TDEAchievements.steel_upgrade);
 		}
 	}
-	
+
 	@SubscribeEvent
-	public void onItemPickup(ItemCraftedEvent event) {
+	public void onItemPickup(ItemPickupEvent event){
 		EntityPlayer player = event.player;
-		Item item = event.crafting.getItem();
+		Item item = event.pickedUp.getEntityItem().getItem();
 		
-		if (item.equals(TDETools.ancient_pickaxe) || item.equals(TDETools.ancient_axe) || item.equals(TDETools.ancient_shovel) || item.equals(TDETools.ancient_hoe)) {
-			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Ancient Upgrade!"));
-			AchievementHandler.setAchieved(TDEAchievements.ancient_upgrade);
+		if(item.equals(TDETools.ancient_pickaxe)){
+			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Stone Upgrade!"));
+			AchievementHandler.setAchieved(TDEAchievements.stone_upgrade);
 		}
 	}
+	
+	
 
 }
