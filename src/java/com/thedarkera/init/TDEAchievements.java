f7e91baba10343b50709802e0c6402c458a412cd
iMrBlurr <imrblurr@gmail.com>
2015-05-28 16:30:16 +0100

    Added achievement categories on desc

--- src/java/com/thedarkera/init/TDEAchievements.java
+++ src/java/com/thedarkera/init/TDEAchievements.java
@@ -6,44 +6,43 @@ import com.thedarkera.utils.Achievement;
 import java.util.Arrays;
 import java.util.List;
 
+import net.minecraft.util.EnumChatFormatting;
+
 public class TDEAchievements {
 
-	/*
-	public static Achievement test = new Achievement("test1","test1");
-	public static Achievement test2 = new Achievement("test2","test2");
-	public static Achievement test3 = new Achievement("test3","test3");
-	public static Achievement test4 = new Achievement("test4","test4");
-	public static Achievement test5 = new Achievement("test5","test5");
-	public static Achievement test6 = new Achievement("test6","test6");
+	/**
+	 * Biomes = Dark Green
+	 * Combat = Blue
+	 * Tools = Aqua
+	 * Dimensions = Grey
+	 * Structures = Green
+	 * Crafting = Yellow	
 	*/
 	
-    public static Achievement village = new Achievement("Dark Village", Arrays.asList("Locate the Dark Village! This is the beginning!"));
-    public static Achievement travelling = new Achievement("The Dark", Arrays.asList("Teleport To The Dark!"));
-    public static Achievement stone_upgrade = new Achievement("Stone Upgrade", Arrays.asList("Upgrade to Dark Stone Tools."));
-    public static Achievement steel_upgrade = new Achievement("Steel Upgrade", Arrays.asList("Upgrade to Steel Tools."));
-    public static Achievement ancient_upgrade = new Achievement("Ancient Upgrade", Arrays.asList("Get a fall set of Ancient Tools."));
-    public static Achievement dragon_slayer = new Achievement("Dragon Slayer", Arrays.asList("Kill a dragon!"));
-    
-    public static Achievement workbench = new Achievement("Workbench Crafter", Arrays.asList("Make a workbench"));
-    public static Achievement smeltery = new Achievement("Dark Smelter", Arrays.asList("Make a smeltery"));
-    public static Achievement forge = new Achievement("Armor Crafter", Arrays.asList("Make a smithing forge"));
-    public static Achievement grind_stone = new Achievement("Tool Crafter", Arrays.asList("Make a grind stone"));
-    public static Achievement anvil = new Achievement("Tool Upgrader", Arrays.asList("Make a dark anvil"));
-    public static Achievement alchemy_table = new Achievement("Potion Mixer", Arrays.asList("Make a alchemy table"));
-    public static Achievement rune_table = new Achievement("Rune Enchanter", Arrays.asList("Make a rune table"));
-    
-    //Biomes	
-    public static Achievement biome = new Achievement("Dark Forrest", Arrays.asList("Find the Dark Forrest Biome"));
-    public static Achievement biome1 = new Achievement("Dark Jungle", Arrays.asList("Find the Dark Jungle Biome"));
-    public static Achievement biome2 = new Achievement("Dark Plains", Arrays.asList("Find the Dark Plains Biome"));
-    public static Achievement biome3 = new Achievement("Dark Hills", Arrays.asList("Find the Dark Hills Biome"));
-    public static Achievement biome4 = new Achievement("Dead Land", Arrays.asList("Find the Dead Land Biome"));
-    public static Achievement biome5 = new Achievement("Dark Ocean", Arrays.asList("Find the Dark Ocean Biome"));
-    public static Achievement biome6= new Achievement("Dark Land", Arrays.asList("Find the Dark Land Biome"));
-    public static Achievement biome7 = new Achievement("Ancient Land", Arrays.asList("Find the Ancient Land Biome"));
-    public static Achievement biome8 = new Achievement("Misty Mountains", Arrays.asList("Find the Misty Mountains Biome", "Located inside the Dark Hills biome only"));
-    public static Achievement biome9 = new Achievement("Magical Forrest", Arrays.asList("Find the Magical Forrest Biome"));
-    public static Achievement biome10 = new Achievement("Roofed Forrest", Arrays.asList("Find the Dark Roofed Forrest", "Located inside the Dark Forrest biome only"));
+    public static Achievement village = new Achievement("Dark Village", Arrays.asList("Locate the Dark Village", EnumChatFormatting.GREEN + "Structures"));
+    public static Achievement travelling = new Achievement("The Dark", Arrays.asList("Teleport To The Dark", EnumChatFormatting.GRAY + "Dimensions"));
+    public static Achievement stone_upgrade = new Achievement("Stone Upgrade", Arrays.asList("Upgrade to Dark Stone Tools", EnumChatFormatting.AQUA + "Tools"));
+    public static Achievement steel_upgrade = new Achievement("Steel Upgrade", Arrays.asList("Upgrade to Steel Tools", EnumChatFormatting.AQUA + "Tools"));
+    public static Achievement ancient_upgrade = new Achievement("Ancient Upgrade", Arrays.asList("Find a Ancient Tool", EnumChatFormatting.AQUA + "Tools"));
+    public static Achievement dragon_slayer = new Achievement("Dragon Slayer", Arrays.asList("Kill a dragon", EnumChatFormatting.BLUE + "Combat"));
+    public static Achievement workbench = new Achievement("Workbench Crafter", Arrays.asList("Make a workbench", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement smeltery = new Achievement("Dark Smelter", Arrays.asList("Make a smeltery", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement forge = new Achievement("Armor Crafter", Arrays.asList("Make a smithing forge", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement grind_stone = new Achievement("Tool Crafter", Arrays.asList("Make a grind stone", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement anvil = new Achievement("Tool Upgrader", Arrays.asList("Make a dark anvil", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement alchemy_table = new Achievement("Potion Mixer", Arrays.asList("Make a alchemy table", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement rune_table = new Achievement("Rune Enchanter", Arrays.asList("Make a rune table", EnumChatFormatting.GOLD + "Crafting"));
+    public static Achievement biome = new Achievement("Dark Forrest", Arrays.asList("Find the Dark Forrest Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome1 = new Achievement("Dark Jungle", Arrays.asList("Find the Dark Jungle Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome2 = new Achievement("Dark Plains", Arrays.asList("Find the Dark Plains Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome3 = new Achievement("Dark Hills", Arrays.asList("Find the Dark Hills Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome4 = new Achievement("Dead Land", Arrays.asList("Find the Dead Land Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome5 = new Achievement("Dark Ocean", Arrays.asList("Find the Dark Ocean Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome6= new Achievement("Dark Land", Arrays.asList("Find the Dark Land Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome7 = new Achievement("Ancient Land", Arrays.asList("Find the Ancient Land Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome8 = new Achievement("Misty Mountains", Arrays.asList("Find the Misty Mountains Biome", EnumChatFormatting.DARK_RED + "Located inside the Dark Hills biome only", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome9 = new Achievement("Magical Forrest", Arrays.asList("Find the Magical Forrest Biome", EnumChatFormatting.DARK_GREEN + "Biomes"));
+    public static Achievement biome10 = new Achievement("Roofed Forrest", Arrays.asList("Find the Dark Roofed Forrest", EnumChatFormatting.DARK_RED + "Located inside the Dark Forrest biome only", EnumChatFormatting.DARK_GREEN + "Biomes"));
     
     
     public static void init(){
