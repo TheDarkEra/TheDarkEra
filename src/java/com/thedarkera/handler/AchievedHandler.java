f785dce4a60f389a7ffbb8da3e6aa6c2335e69f0
iMrBlurr <imrblurr@gmail.com>
2015-05-28 16:42:10 +0100

    Removed chat message
    As it comes up everytime you complete the challenge not just first time

--- src/java/com/thedarkera/handler/AchievedHandler.java
+++ src/java/com/thedarkera/handler/AchievedHandler.java
@@ -21,12 +21,10 @@ public class AchievedHandler {
 		Item item = event.crafting.getItem();
 
 		if (item.equals(TDETools.dark_stone_pickaxe) || item.equals(TDETools.dark_stone_axe) || item.equals(TDETools.dark_stone_shovel) || item.equals(TDETools.dark_stone_hoe) || item.equals(TDEWeapons.dark_stone_sword)) {
-			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Stone Upgrade!"));
 			AchievementHandler.setAchieved(TDEAchievements.stone_upgrade);
 		}
 
 		if (item.equals(TDETools.steel_pickaxe) || item.equals(TDETools.steel_axe) || item.equals(TDETools.steel_shovel) || item.equals(TDETools.steel_hoe) || item.equals(TDEWeapons.steel_sword)) {
-			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Steel Upgrade!"));
 			AchievementHandler.setAchieved(TDEAchievements.steel_upgrade);
 		}
 	}
@@ -37,7 +35,6 @@ public class AchievedHandler {
 		Item item = event.pickedUp.getEntityItem().getItem();
 		
 		if(item.equals(TDETools.ancient_pickaxe) || item.equals(TDETools.ancient_axe) || item.equals(TDETools.ancient_shovel) || item.equals(TDETools.ancient_hoe) || item.equals(TDEWeapons.ancient_dagger) || item.equals(TDEWeapons.ancient_sword)){
-			player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "Achievement Get: " + EnumChatFormatting.DARK_RED + "Stone Upgrade!"));
 			AchievementHandler.setAchieved(TDEAchievements.ancient_upgrade);
 		}
 	}
