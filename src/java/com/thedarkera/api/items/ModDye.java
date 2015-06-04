d78d3587b974a7a6f123899de2655a22d30965cf
iMrBlurr <imrblurr@gmail.com>
2015-06-04 15:27:13 +0100

    meta for dark dye

--- src/java/com/thedarkera/api/items/ModDye.java
+++ src/java/com/thedarkera/api/items/ModDye.java
@@ -1,16 +1,49 @@
 package com.thedarkera.api.items;
 
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
 import com.thedarkera.TheDarkEra;
 
 public class ModDye extends ModItem{
 
-	public ModDye(String name, String color, int id) {
+	public ModDye(String name) {
 		super(name);
-		String ItemId = name + "." + id;
-		String resource = TheDarkEra.MODID + ":" + name + "_" + color;
-		
-		setUnlocalizedName(ItemId);
-		setTextureName(resource);
+		setHasSubtypes(true);
+	}
+	
+	public IIcon[] icons = new IIcon[2];
+
+	@Override
+	public void registerIcons(IIconRegister reg) {
+	    for (int i = 0; i < 2; i ++) {
+	        this.icons[i] = reg.registerIcon(TheDarkEra.MODID + ":" + "dark_dye_" + i);
+	    }
 	}
 
+	@Override
+	public IIcon getIconFromDamage(int meta) {
+	    if (meta > 1)
+	        meta = 0;
+
+	    return this.icons[meta];
+	}
+
+	@Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+	    for (int i = 0; i < 2; i ++) {
+	        list.add(new ItemStack(item, 1, i));
+	    }
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+	    return this.getUnlocalizedName() + "_" + stack.getItemDamage();
+	}
+	
 }
