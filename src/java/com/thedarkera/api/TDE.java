28f23615611c9ab8b523d68e19226c1cee105548
iMrBlurr <imrblurr@gmail.com>
2015-06-08 18:08:41 +0100

    tab images
    for some reason not loading

--- src/java/com/thedarkera/api/TDE.java
+++ src/java/com/thedarkera/api/TDE.java
@@ -2,7 +2,6 @@ package com.thedarkera.api;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 
 import com.thedarkera.init.TDEItems;
 
@@ -11,12 +10,15 @@ public class TDE {
 	public static CreativeTabs tabTDEBlocks = new CreativeTabs(CreativeTabs.getNextID(), "the_dark_era_blocks") {
 		@Override
 		public Item getTabIconItem() {
+			this.setBackgroundImageName("tabTDEBlocks");
 			return TDEItems.tab_logo_0;
 		}
 	};
+	
 	public static CreativeTabs tabTDEItems = new CreativeTabs(CreativeTabs.getNextID(), "the_dark_era_items") {
 		@Override
 		public Item getTabIconItem() {
+			this.setBackgroundImageName("tabTDEItems");
 			return TDEItems.tab_logo_1;
 		}
 	};
