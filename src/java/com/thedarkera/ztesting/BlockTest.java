303f835c1026c219e6c9f13d0348edfb85327315
iMrBlurr <imrblurr@gmail.com>
2015-06-08 14:46:01 +0100

    Adding textures and testing my sync

---
+++ src/java/com/thedarkera/ztesting/BlockTest.java
@@ -0,0 +1,15 @@
+package com.thedarkera.ztesting;
+
+import net.minecraft.block.material.Material;
+
+import com.thedarkera.api.blocks.BlockTDEBase;
+
+public class BlockTest extends BlockTDEBase {
+
+	public BlockTest(String name, Material material, float hardness, float resistance, String toolClass, int harvestLevel, SoundType stepSound)	{
+		super(name, material, hardness, resistance, toolClass, harvestLevel, stepSound);
+	}
+	
+	// Simply just making sure my workspace is working with git! :) Hopefully this syncs :P
+
+}
