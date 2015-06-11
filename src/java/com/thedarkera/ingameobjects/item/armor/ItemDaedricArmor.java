2d6fe7b79bf232990c65a0e48b24df8306130a12
iMrBlurr <imrblurr@gmail.com>
2015-06-11 14:31:12 +0100

    Fixed helmet error

--- src/java/com/thedarkera/ingameobjects/item/armor/ItemDaedricArmor.java
+++ src/java/com/thedarkera/ingameobjects/item/armor/ItemDaedricArmor.java
@@ -13,25 +13,22 @@ import net.minecraft.world.World;
 import com.thedarkera.TheDarkEra;
 import com.thedarkera.api.TDE;
 import com.thedarkera.init.TDEArmors;
-import com.thedarkera.proxy.CommonProxy;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemDaedricArmor extends ItemTDEArmor {
-	public ItemDaedricArmor(ArmorMaterial material, int armorType, String name) {
-		super(material, 0, armorType);
+
+	public ItemDaedricArmor(ArmorMaterial material, int par2, int armorType, String name) {
+		super(material, par2, armorType, name);
 		setUnlocalizedName(name);
 		setTextureName(TheDarkEra.MODID + ":" + name);
 		setCreativeTab(TDE.tabTDEItems);
 	}
 
 	@Override
-	public String getArmorTexture(ItemStack stack, Entity entity, int slot,
-			String type) {
-		if (stack.getItem() == TDEArmors.daedric_helmet
-				|| stack.getItem() == TDEArmors.daedric_chestplate
-				|| stack.getItem() == TDEArmors.daedric_boots) {
+	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type) {
+		if (stack.getItem() == TDEArmors.daedric_helmet || stack.getItem() == TDEArmors.daedric_chestplate || stack.getItem() == TDEArmors.daedric_boots) {
 			return TheDarkEra.MODID + ":models/armor/daedric_armor_1.png";
 		} else if (stack.getItem() == TDEArmors.daedric_leggings) {
 			return TheDarkEra.MODID + ":models/armor/daedric_armor_2.png";
@@ -40,45 +37,32 @@ public class ItemDaedricArmor extends ItemTDEArmor {
 			return null;
 		}
 	}
-	@SideOnly(Side.CLIENT)
-	   // @Override
-	    public ModelBiped getArmorModel(EntityLiving entityLiving, ItemStack itemStack, int armorSlot) {
-	    	ModelBiped armorModel = null;
-	    		if(itemStack != null) {
-	    			if(itemStack.getItem() instanceof ItemDaedricArmor) {
-	    				
-	    			}
-	    		}
-	    		
-	    	int type =((ItemArmor)itemStack.getItem()).armorType;
-	    		if(type == 1 || type == 3) {
-	    			armorModel = TheDarkEra.proxy.getArmorModel(0);
-	    		}
-	    		else {
-	    			armorModel = TheDarkEra.proxy.getArmorModel(1);
-	    		}
-//	    	    if (armorSlot == 0)
-//	           return new ModelScaleHelm();
-	        return null;
-	    }
 
+	@SideOnly(Side.CLIENT)
+	// @Override
+	public ModelBiped getArmorModel(EntityLiving entityLiving, ItemStack itemStack, int armorSlot) {
+		ModelBiped armorModel = null;
+		if (itemStack != null) {
+			if (itemStack.getItem() instanceof ItemDaedricArmor) {
+
+			}
+		}
+
+		int type = ((ItemArmor) itemStack.getItem()).armorType;
+		if (type == 1 || type == 3) {
+			armorModel = TheDarkEra.proxy.getArmorModel(0);
+		} else {
+			armorModel = TheDarkEra.proxy.getArmorModel(1);
+		}
+		//	    	    if (armorSlot == 0)
+		//	           return new ModelScaleHelm();
+		return null;
+	}
 
 	@Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
-		if (player.getCurrentArmor(0) != null
-				&& player.getCurrentArmor(0).getItem()
-						.equals(TDEArmors.daedric_boots)
-				&& player.getCurrentArmor(1) != null
-				&& player.getCurrentArmor(1).getItem()
-						.equals(TDEArmors.daedric_leggings)
-				&& player.getCurrentArmor(2) != null
-				&& player.getCurrentArmor(2).getItem()
-						.equals(TDEArmors.daedric_chestplate)
-				&& player.getCurrentArmor(3) != null
-				&& player.getCurrentArmor(3).getItem()
-						.equals(TDEArmors.daedric_helmet)) {
-			player.addPotionEffect(new PotionEffect(Potion.damageBoost.id,
-					20 * 1));
+		if (player.getCurrentArmor(0) != null && player.getCurrentArmor(0).getItem().equals(TDEArmors.daedric_boots) && player.getCurrentArmor(1) != null && player.getCurrentArmor(1).getItem().equals(TDEArmors.daedric_leggings) && player.getCurrentArmor(2) != null && player.getCurrentArmor(2).getItem().equals(TDEArmors.daedric_chestplate) && player.getCurrentArmor(3) != null && player.getCurrentArmor(3).getItem().equals(TDEArmors.daedric_helmet)) {
+			player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20 * 1));
 			player.addPotionEffect(new PotionEffect(Potion.digSpeed.id, 20 * 1));
 		}
 	}
--- src/java/com/thedarkera/ingameobjects/item/armor/ItemTDEArmor.java
+++ src/java/com/thedarkera/ingameobjects/item/armor/ItemTDEArmor.java
@@ -1,11 +1,5 @@
 package com.thedarkera.ingameobjects.item.armor;
 
-import com.thedarkera.TheDarkEra;
-import com.thedarkera.api.TDE;
-import com.thedarkera.init.TDEArmors;
-import com.thedarkera.init.TDEItems;
-import com.thedarkera.utils.IBuffHandler;
-
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
@@ -14,81 +8,81 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-public class ItemTDEArmor extends ItemArmor{
-	
+import com.thedarkera.TheDarkEra;
+import com.thedarkera.api.TDE;
+import com.thedarkera.init.TDEArmors;
+import com.thedarkera.utils.IBuffHandler;
+
+public class ItemTDEArmor extends ItemArmor {
+
 	private IBuffHandler buffHandler;
-	
-	
-	public ItemTDEArmor(ArmorMaterial material, int armorType, String name) {
-		super(material, 0, armorType);
+
+	public ItemTDEArmor(ArmorMaterial material, int par2, int armorType, String name) {
+		super(material, par2, armorType);
 		setUnlocalizedName(name);
 		setTextureName(TheDarkEra.MODID + ":" + getUnlocalizedName().substring(5));
 		setCreativeTab(TDE.tabTDEItems);
 	}
+
 	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type) {
-		if(stack.getItem() == TDEArmors.daedric_helmet || stack.getItem() == TDEArmors.daedric_chestplate || stack.getItem() == TDEArmors.daedric_boots) {
+		if (stack.getItem() == TDEArmors.daedric_helmet || stack.getItem() == TDEArmors.daedric_chestplate || stack.getItem() == TDEArmors.daedric_boots) {
 			return TheDarkEra.MODID + ":modles/armor/daedric_layer1";
-		}
-		else if (stack.getItem() == TDEArmors.daedric_leggings) {
+		} else if (stack.getItem() == TDEArmors.daedric_leggings) {
 			return TheDarkEra.MODID + ":modles/armor/daedric_layer2";
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
-	
+
 	@Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
-		
-		if(player.getActivePotionEffects().contains(Potion.damageBoost.id) && this instanceof ItemDaedricArmor) {
-		    ItemStack[] armor = new ItemStack[] {
-		    		new ItemStack(TDEArmors.daedric_boots),
-		    		new ItemStack(TDEArmors.daedric_chestplate),
-		    		new ItemStack(TDEArmors.daedric_leggings),
-		    		new ItemStack(TDEArmors.daedric_helmet)
-		    		}; //Create a new itemstack with each piece of your armor
-		int gearCounter = 0;
-		for(int i = 1; i < 5; i++) {
-		     if(player.getEquipmentInSlot(i) == armor[i]) {
-		        gearCounter++;
-		     }
-		}
-		if(gearCounter == 3) {
-		    player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 0));
-		    player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 20, 1));
-		    player.addPotionEffect(new PotionEffect(Potion.resistance.id, 20, 0));
-		    gearCounter = 0;
-		}
-	
-		/*
-		if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.armorSet == true) {
-			if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.scaleArmorSet == true) {
-	        player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 0));
-	        player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 20, 1));
-	        player.addPotionEffect(new PotionEffect(Potion.resistance.id, 20, 0));
-		}
-		else if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.thiefArmorSet == true) {
-	        player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 0));
-	        player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 21 * 20, 0));
-	        player.capabilities.setPlayerWalkSpeed(0.15F);
-	        player.jumpMovementFactor = player.capabilities.getWalkSpeed() * 0.5F;
-		}
-		else if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.tribalArmorSet) {
-	        player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 0));
-	        player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 21 * 20, 0));
-	        player.addPotionEffect(new PotionEffect(Potion.jump.id, 2 * 20, 2));
-		}
-			
-		else { System.out.println("Error");
-	}
-	
-	*/
-	/*
-			 //    	player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 40));
+
+		if (player.getActivePotionEffects().contains(Potion.damageBoost.id) && this instanceof ItemDaedricArmor) {
+			ItemStack[] armor = new ItemStack[] { new ItemStack(TDEArmors.daedric_boots), new ItemStack(TDEArmors.daedric_chestplate), new ItemStack(TDEArmors.daedric_leggings), new ItemStack(TDEArmors.daedric_helmet) }; //Create a new itemstack with each piece of your armor
+			int gearCounter = 0;
+			for (int i = 1; i < 5; i++) {
+				if (player.getEquipmentInSlot(i) == armor[i]) {
+					gearCounter++;
 				}
-*/
+			}
+			if (gearCounter == 3) {
+				player.addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 0));
+				player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 20, 1));
+				player.addPotionEffect(new PotionEffect(Potion.resistance.id, 20, 0));
+				gearCounter = 0;
+			}
+
+			/*
+			 * if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.armorSet
+			 * == true) {
+			 * if(net.kloudspace.kloudgear.util.core.ability.ArmorSet.
+			 * scaleArmorSet == true) { player.addPotionEffect(new
+			 * PotionEffect(Potion.damageBoost.id, 20, 0));
+			 * player.addPotionEffect(new PotionEffect(Potion.fireResistance.id,
+			 * 20, 1)); player.addPotionEffect(new
+			 * PotionEffect(Potion.resistance.id, 20, 0)); } else
+			 * if(net.kloudspace
+			 * .kloudgear.util.core.ability.ArmorSet.thiefArmorSet == true) {
+			 * player.addPotionEffect(new PotionEffect(Potion.damageBoost.id,
+			 * 20, 0)); player.addPotionEffect(new
+			 * PotionEffect(Potion.nightVision.id, 21 * 20, 0));
+			 * player.capabilities.setPlayerWalkSpeed(0.15F);
+			 * player.jumpMovementFactor = player.capabilities.getWalkSpeed() *
+			 * 0.5F; } else
+			 * if(net.kloudspace.kloudgear.util.core.ability.ArmorSet
+			 * .tribalArmorSet) { player.addPotionEffect(new
+			 * PotionEffect(Potion.damageBoost.id, 20, 0));
+			 * player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 21
+			 * * 20, 0)); player.addPotionEffect(new
+			 * PotionEffect(Potion.jump.id, 2 * 20, 2)); }
+			 * 
+			 * else { System.out.println("Error"); }
+			 */
+			/*
+			 * // player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id,
+			 * 40)); }
+			 */
 		}
 	}
 }
-
