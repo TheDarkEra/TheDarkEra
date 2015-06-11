64b14d7cf42916be005157055a3a53270764d50d
BubbleTrouble14 <ronald.goedeke@outlook.com>
2015-06-11 15:36:56 +0200

    Merge conflict

--- src/java/com/thedarkera/ingameobjects/item/armor/ItemDaedricArmor.java
+++ src/java/com/thedarkera/ingameobjects/item/armor/ItemDaedricArmor.java
@@ -19,24 +19,25 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemDaedricArmor extends ItemTDEArmor {
 
-	public ItemDaedricArmor(ArmorMaterial material, int par2, int armorType, String name) {
-		super(material, par2, armorType, name);
-		setUnlocalizedName(name);
+	public ItemDaedricArmor(String unlocalizedName, ArmorMaterial material, int armorType, String name) {
+		super(material, armorType, name);
+		this.setUnlocalizedName(unlocalizedName);
+		this.textureName = name;
 		setTextureName(TheDarkEra.MODID + ":" + name);
 		setCreativeTab(TDE.tabTDEItems);
 	}
 
 	@Override
 	public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type) {
-		if (stack.getItem() == TDEArmors.daedric_helmet || stack.getItem() == TDEArmors.daedric_chestplate || stack.getItem() == TDEArmors.daedric_boots) {
-			return TheDarkEra.MODID + ":models/armor/daedric_armor_1.png";
-		} else if (stack.getItem() == TDEArmors.daedric_leggings) {
-			return TheDarkEra.MODID + ":models/armor/daedric_armor_2.png";
-		} else {
-			TheDarkEra.logger.warn("Invalid armor item!");
-			return null;
-		}
-	}
+   //     if (slot == 0)
+     //       return ;
+      //  else {
+        	return TheDarkEra.MODID + ":armor/" + textureName + "_" + (this.armorType == 2 ? "2" : "1") +".png";
+        }
+		
+		//	return Reference.MODID + ":armor/" + this.textureName + "_" + (this.armorType == 2 ? "2" : "1") +".png";
+//	}
+
 
 	@SideOnly(Side.CLIENT)
 	// @Override
